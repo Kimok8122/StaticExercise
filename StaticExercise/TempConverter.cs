ee23d24974967ae2b6a3d4e15aa3b20a020ed4c6
Mona Lisa <email@example.com>
2020-09-04 10:09:26 -0600

    starting

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double c)
+        {
+            var cresult = (c - 32) / 1.8;
+            return cresult;
+        }
+
+        public static double CelsiusToFahrenheit(double f)
+        {
+            return (f * 9) / 5 + 32;
+        }
+
+    }
+}
